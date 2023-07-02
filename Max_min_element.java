66d324fbdf38717a99f4e95e5fe77611b3fbcc1f
Ankush <sounankush123@gmail.com>
2023-07-02 11:00:20 +0200

    commit

--- Max_min_element.java
+++ Max_min_element.java
@@ -1,6 +1,13 @@
+import java.util.*;
 public class Max_min_element {
     public static void maxmin(int arr[]) {
-        
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        for(int i = 0; i < arr.length; i++) {
+            max = Integer.max(max, arr[i]);
+            min = Integer.min(min, arr[i]);
+        }
+        System.out.print("Max is : " + max + "\nMin is : " + min);
     }
     public static void main(String args[]) {
         int arr[] = {6,2,1,4,56,2};
