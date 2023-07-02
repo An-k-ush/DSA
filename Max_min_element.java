0681f146fa34c6a5dc24451c800fd62b23211332
Ankush <sounankush123@gmail.com>
2023-07-02 10:49:57 +0200

    commit

---
+++ Max_min_element.java
@@ -0,0 +1,9 @@
+public class Max_min_element {
+    public static void maxmin(int arr[]) {
+        
+    }
+    public static void main(String args[]) {
+        int arr[] = {6,2,1,4,56,2};
+        maxmin(arr);
+    }
+}
